b6f209c61da7c4f501d6a1eaa528da16b71084b0
mmicromegas <34376626+mmicromegas@users.noreply.github.com>
2018-03-24 21:53:43 +0100

    improvements

--- PROMPI_RANS_xnu.py
+++ PROMPI_RANS_xnu.py
@@ -4,35 +4,51 @@ import CALCULUS as calc
 
 class PROMPI_xnu(calc.CALCULUS,object):
 
-    def __init__(self,filename,inuc,intc):
+    def __init__(self,filename,inuc,intc,LGRID):
         super(PROMPI_xnu,self).__init__(filename) 
 	
         # load data to structured array
         eht = np.load(filename)	
 		
+        self.lgrid = LGRID
         self.intc = intc
 		
         # assign data and convert it to numpy array
         self.timec     = eht.item().get('timec')[intc] 
         self.xzn0      = np.asarray(eht.item().get('rr')) 
         self.dd        = np.asarray(eht.item().get('dd')[intc])
-        self.ddxi      = np.asarray(eht.item().get('ddx'+inuc)[intc])
+        self.pp        = np.asarray(eht.item().get('pp')[intc])
         self.ddux      = np.asarray(eht.item().get('ddux')[intc])
+        self.dduy      = np.asarray(eht.item().get('dduy')[intc])
+        self.dduz      = np.asarray(eht.item().get('dduz')[intc])				
+        self.dduxux    = np.asarray(eht.item().get('dduxux')[intc])
+        self.dduyuy    = np.asarray(eht.item().get('dduyuy')[intc])
+        self.dduzuz    = np.asarray(eht.item().get('dduzuz')[intc])
+        self.xi        = np.asarray(eht.item().get('x'+inuc)[intc])		
+        self.ddxi      = np.asarray(eht.item().get('ddx'+inuc)[intc])
         self.ddxiux    = np.asarray(eht.item().get('ddx'+inuc+'ux')[intc])
         self.ddxidot   = np.asarray(eht.item().get('ddx'+inuc+'dot')[intc])	
         self.ddxisq    = np.asarray(eht.item().get('ddx'+inuc+'sq')[intc])
         self.ddxisqux  = np.asarray(eht.item().get('ddx'+inuc+'squx')[intc])
-        self.ddxixidot = np.asarray(eht.item().get('ddx'+inuc+'x'+inuc+'dot')[intc]) 		
+        self.ddxixidot = np.asarray(eht.item().get('ddx'+inuc+'x'+inuc+'dot')[intc])
+        self.xigradxpp = np.asarray(eht.item().get('x'+inuc+'gradxpp')[intc]) 	 		
+        self.ddxidotux = np.asarray(eht.item().get('ddx'+inuc+'dotux')[intc]) 	
+        self.ddxiuxux  = np.asarray(eht.item().get('ddx'+inuc+'uxux')[intc])		
+        self.ddxiuyuy  = np.asarray(eht.item().get('ddx'+inuc+'uyuy')[intc])
+        self.ddxiuzuz  = np.asarray(eht.item().get('ddx'+inuc+'uzuz')[intc])		
+	
 		
         # store time series for time derivatives
-        self.t_timec       = eht.item().get('timec') 
+        self.t_timec   = eht.item().get('timec') 
         self.t_dd      = eht.item().get('dd') 
-        self.t_ddux      = eht.item().get('ddux') 		
+        self.t_ddux    = eht.item().get('ddux') 		
         self.t_ddxi    = np.asarray(eht.item().get('ddx'+inuc))		
         self.t_ddxisq  = np.asarray(eht.item().get('ddx'+inuc+'sq'))
+        self.t_ddxiux    = np.asarray(eht.item().get('ddx'+inuc+'ux'))
 		
         #######################
         # Xi TRANSPORT EQUATION 
+        #######################
 		
         # LHS dq/dt 		
         self.dtddxi = self.dt(self.t_ddxi,self.xzn0,self.t_timec,intc)
@@ -46,11 +62,13 @@ class PROMPI_xnu(calc.CALCULUS,object):
         # res
         self.resXiTransport = - self.dtddxi - self.divddxiux - self.divfxi + self.ddXidot
 		
+        ###########################		
         # END Xi TRANSPORT EQUATION
-        #######################
+        ###########################
 		
         ######################
         # Xi VARIANCE EQUATION 
+        ######################		
  
         # LHS dq/dt 
         self.t_ddsigmai = self.t_ddxisq -self.t_ddxi*self.t_ddxi/self.t_dd
@@ -72,9 +90,50 @@ class PROMPI_xnu(calc.CALCULUS,object):
         self.resXiVariance = - self.dtddsigmai - self.divdduxsigmai - \
                              self.divfxir - self.fxigradi + self.xifddxidot
 	
-        ######################
-        # Xi VARIANCE EQUATION 		
+        ##########################
+        # END Xi VARIANCE EQUATION 		
+        ##########################		
 		
+        ##################
+        # Xi FLUX EQUATION 
+        ##################		
+ 
+        # LHS dq/dt 
+        self.t_ddfluxi = self.t_ddxiux -self.t_ddxi*self.t_ddux/self.t_dd
+        self.dtfluxi = self.dt(self.t_ddfluxi,self.xzn0,self.t_timec,intc)
+        # LHS div(dduxfluxi)
+        self.divuxfluxi = self.Div((self.ddxiux -self.ddxi*self.ddux/self.dd)*(self.ddux/self.dd),self.xzn0)		
+
+        # RHS div(frxi) 
+        self.divfrxi = self.Div(self.ddxiuxux - (self.ddxi/self.dd)*self.dduxux - \
+                      2.*(self.ddux/self.dd)*self.ddxiux + \
+                      2.*self.ddxi*self.ddux*self.ddux/(self.dd*self.dd),self.xzn0)
+
+        # RHS fi d_r fu_r
+        self.figradxfur = (self.ddxiux - self.ddxi*self.ddux/self.dd)*self.Grad(self.ddux/self.dd,self.xzn0)
+		
+        # RHS Rxx d_r Xi
+        self.rxxgradxxi = (self.dduxux - self.ddux*self.ddux/self.dd)*self.Grad(self.ddxi/self.dd,self.xzn0)
+
+        # RHS X''i d_r P - X''_i d_r P'
+        self.xigradp = (self.xi*self.Grad(self.pp,self.xzn0) - (self.ddxi/self.dd)*self.Grad(self.pp,self.xzn0)) + \
+                        (self.xigradxpp - self.xi*self.Grad(self.pp,self.xzn0)) 		
+
+        # RHS uxfddXidot
+        self.uxfddXidot = self.ddxidotux - (self.ddux/self.dd)*self.ddxidot  		
+		
+        # RHS geometry term
+        self.gi = -(self.ddxiuyuy - (self.ddxi/self.dd)*self.dduyuy - 2.*(self.dduy/self.dd) + 2.*self.ddxi*self.dduy*self.dduy/(self.dd*self.dd))/self.xzn0 - \
+                   (self.ddxiuzuz - (self.ddxi/self.dd)*self.dduzuz - 2.*(self.dduz/self.dd) + 2.*self.ddxi*self.dduz*self.dduz/(self.dd*self.dd))/self.xzn0 + \
+                   (self.ddxiuyuy - (self.ddxi/self.dd)*self.dduyuy)/self.xzn0 + \
+                   (self.ddxiuzuz - (self.ddxi/self.dd)*self.dduzuz)/self.xzn0
+		
+        self.resXiFlux = -self.dtfluxi - self.divuxfluxi - self.divfrxi - self.figradxfur - \
+                          self.rxxgradxxi - self.xigradp + self.uxfddXidot + self.gi     
+		
+        ######################
+        # END Xi FLUX EQUATION 
+        ######################	
 		
 		
     def plot_Xrho(self,xbl,xbr,inuc,data_prefix):
@@ -99,7 +158,10 @@ class PROMPI_xnu(calc.CALCULUS,object):
         plt.gca().yaxis.get_major_formatter().set_powerlimits((0,0))		
 		
         # limit x/y axis
-        plt.axis([xbl,xbr,np.min(plt1[idxl:idxr]),np.max(plt1[idxl:idxr])])
+        if self.lgrid == 1:
+            plt.axis([xbl,xbr,np.min(plt1[idxl:idxr]),np.max(plt1[idxl:idxr])])
+        else:
+            plt.axis([grd1[0],grd1[-1],np.min(plt1[0:-1]),np.max(plt1[0:-1])])	
 		
         # plot DATA 
         plt.title('xnucid '+str(xnucid))
@@ -147,9 +209,14 @@ class PROMPI_xnu(calc.CALCULUS,object):
         plt.gca().yaxis.get_major_formatter().set_powerlimits((0,0))		
 		
         # limit x/y axis by global min/max from all terms
-        minx = np.min([np.min(lhs0[idxl:idxr]),np.min(lhs1[idxl:idxr]),np.min(rhs0[idxl:idxr]),np.min(rhs1[idxl:idxr]),np.min(res[idxl:idxr])])
-        maxx = np.max([np.max(lhs0[idxl:idxr]),np.max(lhs1[idxl:idxr]),np.max(rhs0[idxl:idxr]),np.max(rhs1[idxl:idxr]),np.max(res[idxl:idxr])])
-        plt.axis([xbl,xbr,minx,maxx])
+        if self.lgrid == 1:
+            minx = np.min([np.min(lhs0[idxl:idxr]),np.min(lhs1[idxl:idxr]),np.min(rhs0[idxl:idxr]),np.min(rhs1[idxl:idxr]),np.min(res[idxl:idxr])])
+            maxx = np.max([np.max(lhs0[idxl:idxr]),np.max(lhs1[idxl:idxr]),np.max(rhs0[idxl:idxr]),np.max(rhs1[idxl:idxr]),np.max(res[idxl:idxr])])
+            plt.axis([xbl,xbr,minx,maxx])
+        else:
+            minx = np.min([np.min(lhs0[0:-1]),np.min(lhs1[0:-1]),np.min(rhs0[0:-1]),np.min(rhs1[0:-1]),np.min(res[0:-1])])
+            maxx = np.max([np.max(lhs0[0:-1]),np.max(lhs1[0:-1]),np.max(rhs0[0:-1]),np.max(rhs1[0:-1]),np.max(res[0:-1])])			
+            plt.axis([grd1[0],grd1[-1],minx,maxx])
 		
         # plot DATA 
         plt.title('xnucid '+str(xnucid))
@@ -197,8 +264,11 @@ class PROMPI_xnu(calc.CALCULUS,object):
         plt.gca().yaxis.get_major_formatter().set_powerlimits((0,0))
 		
         # limit x/y axis
-        plt.axis([xbl,xbr,np.min(plt1[idxl:idxr]),np.max(plt1[idxl:idxr])])
-		
+        if self.lgrid == 1:
+            plt.axis([xbl,xbr,np.min(plt1[idxl:idxr]),np.max(plt1[idxl:idxr])])
+        else:
+            plt.axis([grd1[0],grd1[-1],np.min(plt1[0:-1]),np.max(plt1[0:-1])])
+			
         # plot DATA 
         plt.title('xnucid '+str(xnucid))
         plt.plot(grd1,plt1,color='k',label = r'f'+str(inuc))
@@ -218,8 +288,89 @@ class PROMPI_xnu(calc.CALCULUS,object):
         # save PLOT
         plt.savefig('RESULTS/'+data_prefix+'mean_Xflux_'+xnucid+'.png')
 		
-    def plot_Xflux_equation(self,xbl,xbr):
-        pass
+    def plot_Xflux_equation(self,xbl,xbr,inuc,data_prefix):
+        """Plot Xi flux equation in the model""" 
+
+        # convert nuc ID to string
+        xnucid = str(inuc)
+		
+        # load x GRID
+        grd1 = self.xzn0
+
+        lhs0 = - self.dtfluxi
+        lhs1 = - self.divuxfluxi
+		
+        rhs0 = - self.divfrxi
+        rhs1 = - self.figradxfur
+        rhs2 = - self.rxxgradxxi
+        rhs3 = - self.xigradp
+        rhs4 = + self.uxfddXidot 
+        rhs5 = + self.gi
+		
+        res =  -self.resXiFlux
+		
+        # calculate INDICES for grid boundaries 
+        idxl, idxr = self.idx_bndry(xbl,xbr)
+		
+        # create FIGURE
+        plt.figure(figsize=(7,6))
+		
+        # format AXIS, make sure it is exponential
+        plt.gca().yaxis.get_major_formatter().set_powerlimits((0,0))		
+		
+        # limit x/y axis by global min/max from all terms
+        if self.lgrid == 1:
+            minx = np.min([np.min(lhs0[idxl:idxr]),np.min(lhs1[idxl:idxr]),\
+	                       np.min(rhs0[idxl:idxr]),np.min(rhs1[idxl:idxr]),\
+	                       np.min(rhs2[idxl:idxr]),np.min(rhs3[idxl:idxr]),\
+	                       np.min(rhs4[idxl:idxr]),np.min(rhs5[idxl:idxr]),\
+	                       np.min(res[idxl:idxr])])
+            maxx = np.max([np.max(lhs0[idxl:idxr]),np.max(lhs1[idxl:idxr]),\
+	                       np.max(rhs0[idxl:idxr]),np.max(rhs1[idxl:idxr]),\
+	                       np.max(rhs2[idxl:idxr]),np.max(rhs3[idxl:idxr]),\
+	                       np.max(rhs4[idxl:idxr]),np.max(rhs5[idxl:idxr]),\
+	                       np.max(res[idxl:idxr])])
+            plt.axis([xbl,xbr,minx,maxx])
+        else:
+            minx = np.min([np.min(lhs0[0:-1]),np.min(lhs1[0:-1]),\
+	                       np.min(rhs0[0:-1]),np.min(rhs1[0:-1]),\
+	                       np.min(rhs2[0:-1]),np.min(rhs3[0:-1]),\
+	                       np.min(rhs4[0:-1]),np.min(rhs5[0:-1]),\
+	                       np.min(res[0:-1])])
+            maxx = np.max([np.max(lhs0[0:-1]),np.max(lhs1[0:-1]),\
+	                       np.max(rhs0[0:-1]),np.max(rhs1[0:-1]),\
+	                       np.max(rhs2[0:-1]),np.max(rhs3[0:-1]),\
+	                       np.max(rhs4[0:-1]),np.max(rhs5[0:-1]),\
+	                       np.max(res[0:-1])])
+            plt.axis([grd1[0],grd1[-1],minx,maxx])		
+		
+        # plot DATA 
+        plt.title('xnucid '+str(xnucid))
+        plt.plot(grd1,lhs0,color='#8B3626',label = r'$-\partial_t f_i$')
+        plt.plot(grd1,lhs1,color='#FF7256',label = r'$-\nabla_r (\widetilde{u}_r f_i)$')		
+        plt.plot(grd1,rhs0,color='b',label=r'$-\nabla_r f^r_i$')
+        plt.plot(grd1,rhs1,color='g',label=r'$-f_i \partial_r \widetilde{u}_r$')
+        plt.plot(grd1,rhs2,color='r',label=r'$-R_{rr} \partial_r \widetilde{X}_i$')	
+        plt.plot(grd1,rhs3,color='cyan',label=r'$-\overline{X^{,,}_i} \partial_r \overline{P} - \overline{X^{,,} \partial_r P^{,}}$')
+        plt.plot(grd1,rhs4,color='purple',label=r'$+\overline{u^{,,}_r \rho \dot{X}_i}$')
+        plt.plot(grd1,rhs5,color='yellow',label=r'$+G_i$')		
+        plt.plot(grd1,res,color='k',linestyle='--',label='res')
+
+        # define and show x/y LABELS
+        setxlabel = r"r (cm)"
+        setylabel = r"g cm$^{-2}$ s$^{-2}$"
+        plt.xlabel(setxlabel)
+        plt.ylabel(setylabel)
+		
+        # show LEGEND
+        plt.legend(loc=1,prop={'size':12})
+
+        # display PLOT
+        plt.show(block=False)
+
+        # save PLOT
+        plt.savefig('RESULTS/'+data_prefix+'mean_Xflux_'+xnucid+'.png')		
+		
 
     def plot_Xvariance(self,xbl,xbr,inuc,data_prefix):
         """Plot Xvariance stratification in the model""" 
@@ -243,7 +394,10 @@ class PROMPI_xnu(calc.CALCULUS,object):
         plt.gca().yaxis.get_major_formatter().set_powerlimits((0,0))
 		
         # limit x/y axis
-        plt.axis([xbl,xbr,1.e-20,np.max(plt1[idxl:idxr])])
+        if self.lgrid == 1:
+            plt.axis([xbl,xbr,1.e-20,np.max(plt1[idxl:idxr])])
+        else:
+            plt.axis([grd1[0],grd1[-1],1.e-20,np.max(plt1[0:-1])])
 		
         # plot DATA 
         plt.title('xnucid '+str(xnucid))
@@ -293,15 +447,21 @@ class PROMPI_xnu(calc.CALCULUS,object):
         plt.gca().yaxis.get_major_formatter().set_powerlimits((0,0))		
 		
         # limit x/y axis by global min/max from all terms
-        minx = np.min([np.min(lhs0[idxl:idxr]),np.min(lhs1[idxl:idxr]),np.min(rhs0[idxl:idxr]),np.min(rhs1[idxl:idxr]),np.min(rhs2[idxl:idxr]),np.min(res[idxl:idxr])])
-        maxx = np.max([np.max(lhs0[idxl:idxr]),np.max(lhs1[idxl:idxr]),np.max(rhs0[idxl:idxr]),np.max(rhs1[idxl:idxr]),np.min(rhs2[idxl:idxr]),np.max(res[idxl:idxr])])
-        plt.axis([xbl,xbr,minx,maxx])
+        if self.lgrid == 1:
+            minx = np.min([np.min(lhs0[idxl:idxr]),np.min(lhs1[idxl:idxr]),np.min(rhs0[idxl:idxr]),np.min(rhs1[idxl:idxr]),np.min(rhs2[idxl:idxr]),np.min(res[idxl:idxr])])
+            maxx = np.max([np.max(lhs0[idxl:idxr]),np.max(lhs1[idxl:idxr]),np.max(rhs0[idxl:idxr]),np.max(rhs1[idxl:idxr]),np.min(rhs2[idxl:idxr]),np.max(res[idxl:idxr])])
+            plt.axis([xbl,xbr,minx,maxx])
+        else:
+            minx = np.min([np.min(lhs0[0:-1]),np.min(lhs1[0:-1]),np.min(rhs0[0:-1]),np.min(rhs1[0:-1]),np.min(rhs2[0:-1]),np.min(res[0:-1])])
+            maxx = np.max([np.max(lhs0[0:-1]),np.max(lhs1[0:-1]),np.max(rhs0[0:-1]),np.max(rhs1[0:-1]),np.min(rhs2[0:-1]),np.max(res[0:-1])])
+            plt.axis([grd1[0],grd1[-1],minx,maxx])		
+		
 		
         # plot DATA 
         plt.title('xnucid '+str(xnucid))
         plt.plot(grd1,lhs0,color='cyan',label = r'$-\partial_t (\overline{\rho} \sigma)$')
         plt.plot(grd1,lhs1,color='purple',label = r'$-\nabla_r (\overline{\rho} \widetilde{u}_r \sigma)$')		
-        plt.plot(grd1,rhs0,color='b',label=r'$-\nabla_r f^r$')
+        plt.plot(grd1,rhs0,color='b',label=r'$-\nabla_r f^\sigma$')
         plt.plot(grd1,rhs1,color='g',label=r'$-2 f \partial_r \widetilde{X}$')
         plt.plot(grd1,rhs2,color='r',label=r'$+2 \overline{\rho X^{,,} \dot{X}}$')		
         plt.plot(grd1,res,color='k',linestyle='--',label='res')
